95ff9d9516885e11be5409bdce152281a6a57037
Sam Rutherford <samuelbrutherford@gmail.com>
2025-03-15 19:16:50 -0500

    Enable specific CORS origins

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      "https://localhost:5073",
+      "https://localhost:3000",
+      "https://localhost:4173",
+      "http://localhost:5073",
+      "http://localhost:3000",
+      "http://localhost:4173",
+    ],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
