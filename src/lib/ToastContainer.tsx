60f7c118e232d3bcc4cc6ea7c844aad7a9a71cd7
Sam Rutherford <samuelbrutherford@gmail.com>
2025-03-22 16:13:13 -0500

    Remove toastcontainer

--- src/lib/ToastContainer.tsx
+++ src/lib/ToastContainer.tsx
@@ -1,35 +1,8 @@
-import { useContext, useEffect } from "react";
-import Toast from "./Toast";
-import { AppDataContext } from "../App";
-
 export type TToast = {
   message: string;
 };
 
 export default function ToastContainer() {
-  const context = useContext(AppDataContext);
-  const { toast, setToast } = context || {};
-
-  if (context === undefined) {
-    throw new Error("useToast must be used within a ToastDataProvider");
-  }
-  const toastMap = toast?.map((item, index) => (
-    <Toast key={index} message={item.message} />
-  ));
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (setToast)
-        if (toast && toast.length > 0) {
-          const copy = [...toast];
-          copy.pop();
-          setToast(copy);
-        }
-    }, 2000);
-
-    return clearTimeout(timer);
-  }, [setToast, toast]);
-
   return (
     <div
       style={{
@@ -42,8 +15,6 @@ export default function ToastContainer() {
         alignItems: "center",
         gap: "0.5rem",
       }}
-    >
-      {toastMap}
-    </div>
+    ></div>
   );
 }
